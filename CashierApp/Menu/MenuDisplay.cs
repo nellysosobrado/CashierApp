2db629a2842ff52a934f4e600033a3eb9d52560c
Nelly <Nelly.sosobrado@gmail.com>
2024-11-06 18:25:44 +0100

    Menu text centered

--- CashierApp/Menu/MenuDisplay.cs
+++ CashierApp/Menu/MenuDisplay.cs
@@ -11,27 +11,36 @@ namespace CashierApp.Menu
         public void ShowMenuOptions(string[] options, int selectedIndex)
         {
             Console.Clear();
-            Console.WriteLine("Use arrow keys and 'Enter' to select a option");
-            Console.WriteLine("\n╔══════════════════════════════════════╗");
+            CenterText("Use arrow keys and 'Enter' to select a option");
+            CenterText("╔══════════════════════════════════════╗");
 
             for (int i = 0; i <options.Length; i++)
             {
+                string line;
                 if( i == selectedIndex)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     //Console.WriteLine($"-> {options[i]}");
-                    Console.WriteLine($"║ -> {options[i].PadRight(30)}    ║");
+                    line = $"║ -> {options[i].PadRight(30)}    ║";
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Gray;
                     //Console.WriteLine($" {options[i]}");
-                    Console.WriteLine($"║    {options[i].PadRight(30)}    ║");
+                    line = $"║ -> {options[i].PadRight(30)}    ║";
                 }
-                
+                CenterText(line);
+
             }
-            Console.WriteLine("╚══════════════════════════════════════╝");
+            CenterText("╚══════════════════════════════════════╝");
             Console.ResetColor();
+        }
+        private void CenterText(string text)
+        {
+            int windowWidth = Console.WindowWidth;
+            int textWidth = text.Length;
+            int padding = (windowWidth - textWidth) / 2;
+            Console.WriteLine(new string(' ', padding) + text);
         }
     }
 }
